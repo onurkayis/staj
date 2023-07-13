c4dd901501533ecd3995459f6a0a4169d7611c62
Onur Kayış <onurrkayis@gmail.com>
2023-07-13 08:39:24 +0300

    Add files via upload

---
+++ WifiConnection.ino
@@ -0,0 +1,35 @@
+#include <WiFi.h>
+
+const char* ssid = "SSTE";
+const char* password = "010A081B0C";
+
+void initWifi(){
+  Serial.print("İnternete bağlanılıyor");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("İnternete bağlanılamadı.");
+  }else{
+    Serial.print("\n");
+    Serial.println("İnternete bağlanıldı.");
+    Serial.print("IP Address: ");
+    Serial.print(WiFi.localIP());
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  initWifi();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
