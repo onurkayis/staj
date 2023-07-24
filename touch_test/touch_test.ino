cb3be06d51d0ef67965092a0e560283796a7db82
Onur Kayış <onurrkayis@gmail.com>
2023-07-24 14:09:37 +0300

    Add files via upload

--- touch_test/touch_test.ino
+++ touch_test/touch_test.ino
@@ -1,1 +1,15 @@
+const int pin12 = 12; //touch pin gpio12
 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Esp32 touch test!");
+}
+
+void loop() {
+  Serial.println(touchRead(pin12));
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
